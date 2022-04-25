99df620b9807aced5984024367557a4740bbc7d6
DeFi NFT <dev@derpland.co>
2022-04-24 22:50:06 -0400

    5c87eb2

---
+++ _nuxt/71a3374.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{350:function(e,n,t){"use strict";t.r(n);var l={name:"IndexPage"},c=t(60),component=Object(c.a)(l,(function(){var e=this,n=e.$createElement;return(e._self._c||n)("h1",[e._v("Welcome to derpland!")])}),[],!1,null,null,null);n.default=component.exports}}]);
